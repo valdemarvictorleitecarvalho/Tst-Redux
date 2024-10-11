b2820a122233cea4396cfe411c63902be02a6b2d
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-10-11 17:22:45 -0300

    Home Difficulty and Stats

--- src/home/script-home.js
+++ src/home/script-home.js
@@ -1,16 +1,20 @@
 function filterQuestions() {
     const searchInput = document.getElementById('searchInput').value.toLowerCase();
     const tagFilter = document.getElementById('tagFilter').value;
+    const difficultyFilter = document.getElementById('difficultyFilter').value;
     const questions = document.querySelectorAll('#questionList li');
 
     questions.forEach((question) => {
         const title = question.querySelector('.question-title').textContent.toLowerCase();
         const tags = question.getAttribute('data-tags');
+        const difficulty = question.getAttribute('data-difficulty'); // Extrai a dificuldade
 
         const matchesSearch = title.includes(searchInput);
         const matchesTag = tagFilter === "" || tags.includes(tagFilter);
+        const matchesDifficulty = difficultyFilter === "" || (difficulty && difficulty === difficultyFilter);
 
-        if (matchesSearch && matchesTag) {
+        // Exibe a questão se todas as condições forem atendidas
+        if (matchesSearch && matchesTag && matchesDifficulty) {
             question.style.display = "";
         } else {
             question.style.display = "none";
